0466707d835a234db17c1303050afb540be5e0e1
Phani Varma Rudraraju <166364678+phanivarma1213@users.noreply.github.com>
2025-07-11 10:51:07 +0530

    Update views.py

--- greeting/views.py
+++ greeting/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 class GreetingView(APIView):
     def post(self, request):
